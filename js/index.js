ea0bf1ffc9ce1265d951115898267a4bdc90c71e
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-05-11 12:45:41 +0300

    Добавляет .prettierrc.json, добавляет 6 задач модуля-01

---
+++ js/index.js
@@ -0,0 +1,65 @@
+// ==========  Задача №1  ==========
+
+// const productName = "Droid";
+// console.log(productName);
+// ("Droid");
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+// 2000;
+
+// ==========  Задача №2  ==========
+
+// let productName = 'Droid';
+// console.log('productName:', productName);
+// let pricePerItem = 2000;
+// console.log('pricePerItem:', pricePerItem);
+// productName = 'Repair droid';
+// console.log('productName:', productName);
+// pricePerItem = pricePerItem + 1500;
+// console.log('pricePerItem:', pricePerItem);
+
+// ==========  Задача №3  ==========
+
+// const topSpeed = 160;
+// console.log('topSpeed:', topSpeed);
+// const distance = 617.54;
+// console.log('distance:', distance);
+// const login = 'mango935';
+// console.log('login:', login);
+// const isOnline = true;
+// console.log('isOnline:', isOnline);
+// const isAdmin = false;
+// console.log('isAdmin:', isAdmin);
+
+// ==========  Задача №4  ==========
+
+// const pricePerItem = 3500;
+// console.log('pricePerItem:', pricePerItem);
+// const orderedQuantity = 4;
+// console.log('orderedQuantity:', orderedQuantity);
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log('totalPrice:', totalPrice);
+
+// ==========  Задача №5  ==========
+
+// const productName = 'Droid';
+// console.log('productName:', productName);
+// const pricePerItem = 3500;
+// console.log('pricePerItem:', pricePerItem);
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits.`;
+// console.log('message:', message);
+
+// ==========  Задача №6  ==========
+
+const pricePerDroid = 800;
+console.log('pricePerDroid:', pricePerDroid);
+const orderedQuantity = 6;
+console.log('orderedQuantity:', orderedQuantity);
+const deliveryFee = 50;
+console.log('deliveryFee:', deliveryFee);
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+console.log('totalPrice:', totalPrice);
+const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+console.log('message:', message);
+
+// ==========  Задача №7  ==========
