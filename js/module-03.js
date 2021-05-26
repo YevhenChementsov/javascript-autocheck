753f9d41bd093d0339e489789bb792fd3853804c
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-05-26 20:10:31 +0300

    Update module-03.js
    Добавляет 10 задач модуля-03

---
+++ js/module-03.js
@@ -0,0 +1,215 @@
+// ==========  Задача №1  ==========
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+// console.log('apartment:', apartment);
+
+// ==========  Задача №2  ==========
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+// console.log('apartment:', apartment);
+
+// ==========  Задача №3  ==========
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+// const aptRating = apartment.rating;
+// const aptDescr = apartment.descr;
+// const aptPrice = apartment.price;
+// const aptTags = apartment.tags;
+// console.log('aptRating:', aptRating);
+// console.log('aptDescr:', aptDescr);
+// console.log('aptPrice:', aptPrice);
+// console.log('aptTags:', aptTags);
+
+// ==========  Задача №4  ==========
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// console.log('ownerName:', ownerName);
+// console.log('ownerPhone:', ownerPhone);
+// console.log('ownerEmail:', ownerEmail);
+// console.log('numberOfTags:', numberOfTags);
+// console.log('firstTag:', firstTag);
+// console.log('lastTag:', lastTag);
+
+// ==========  Задача №5  ==========
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+// console.log('aptRating:', aptRating);
+// console.log('aptDescr:', aptDescr);
+// console.log('aptPrice:', aptPrice);
+// console.log('aptTags:', aptTags);
+
+// ==========  Задача №6  ==========
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Henry Sibola';
+// apartment.tags.push('trusted');
+// console.log('price:', apartment.price);
+// console.log('rating:', apartment.rating);
+// console.log('name:', apartment.owner.name);
+// console.log('tags:', apartment.tags);
+
+// ==========  Задача №7  ==========
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Henry Sibola',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = 'Jamaica';
+// apartment.location.city = 'Kingston';
+// console.log('area:', apartment.area);
+// console.log('rooms:', apartment.rooms);
+// console.log('location:', apartment.location);
+// console.log('country:', apartment.location.country);
+// console.log('city:', apartment.location.city);
+
+// ==========  Задача №8  ==========
+
+// const name = 'Repair Droid';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
+
+// const product = {
+//   name,
+//   price,
+//   image,
+//   tags,
+// };
+// console.log('name:', name);
+// console.log('price:', price);
+// console.log('image:', image);
+// console.log('tags:', tags);
+
+// ==========  Задача №9  ==========
+
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//   [emailInputName]: 'henry.carter@aptmail.com',
+//   [passwordInputName]: 'jqueryismyjam',
+// };
+// console.log('credentials:', credentials);
+
+// ==========  Задача №10  ==========
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log('apartment:', apartment);
+// console.log('keys:', keys);
+// console.log('values:', values);
+
+// ==========  Задача №11  ==========
+// ==========  Задача №12  ==========
+// ==========  Задача №13  ==========
+// ==========  Задача №14  ==========
+// ==========  Задача №15  ==========
+// ==========  Задача №16  ==========
+// ==========  Задача №17  ==========
+// ==========  Задача №18  ==========
+// ==========  Задача №19  ==========
+// ==========  Задача №20  ==========
+// ==========  Задача №21  ==========
+// ==========  Задача №22  ==========
+// ==========  Задача №23  ==========
+// ==========  Задача №24  ==========
+// ==========  Задача №25  ==========
+// ==========  Задача №26  ==========
+// ==========  Задача №27  ==========
+// ==========  Задача №28  ==========
+// ==========  Задача №29  ==========
+// ==========  Задача №30  ==========
+// ==========  Задача №31  ==========
+// ==========  Задача №32  ==========
+// ==========  Задача №33  ==========
+// ==========  Задача №34  ==========
+// ==========  Задача №35  ==========
+// ==========  Задача №36  ==========
+// ==========  Задача №37  ==========
+// ==========  Задача №38  ==========
+// ==========  Задача №39  ==========
+// ==========  Задача №40  ==========
+// ==========  Задача №41  ==========
